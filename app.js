9ce37d99db7f7394c4f3325d622065615bbda171
Beau Elliott <belliott1516@gmail.com>
2022-03-10 16:25:10 -0800

    got DOM and got state

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 // import functions and grab DOM elements
+const waterImgel = document.getElementById('waterImg');
+const skylineImgEl = document.getElementById('skylineImg');
+const natureImgEl = document.getElementById('natureImg');
 
+const welcomeMessage = document.getElementById('welcome-message');
+
+const waterDropdown = document.getElementById('water-dropdown');
+const skylineDropdown = document.getElementById('skyline-dropdown');
+const natureDropdown = document.getElementById('nature-dropdown');
+
+const userCityInput = document.getElementById('user-city-input');
+
+const userSloganInput = document.getElementById('user-slogan-input');
+const sloganButton = document.getElementById('slogan-button');
 // let state
-
+let waterCount = 0;
+let skylineCount = 0;
+let natureCount = 0;
 // set event listeners 
   // get user input
   // use user input to update state 
